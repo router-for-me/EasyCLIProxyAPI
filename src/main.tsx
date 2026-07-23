3bf07df8a2434e0821812662dffea93e4ef4bfe2
iamkunal9 <ks4428560@gmail.com>
2026-07-24 03:26:34 +0530

    feat: add system-aware dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { AppErrorBoundary } from './AppErrorBoundary';
 import App from './App';
 import { I18nProvider } from './i18n';
+import { ThemeProvider } from './theme';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nProvider>
-      <AppErrorBoundary>
-        <App />
-      </AppErrorBoundary>
+      <ThemeProvider>
+        <AppErrorBoundary>
+          <App />
+        </AppErrorBoundary>
+      </ThemeProvider>
     </I18nProvider>
   </StrictMode>
 );
